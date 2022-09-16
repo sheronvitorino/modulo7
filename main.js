01f8563d8c4877633ef47a4c3da381f68a0dbffa
Sheron Silva <sheronvitorino@hotmail.com>
2022-09-16 08:22:22 -0400

    first commit

--- main.js
+++ main.js
@@ -4,17 +4,27 @@ form.addEventListener ('submit', function (e){
 
         var numero1 = parseFloat(document.getElementById('valor-um').value);
         var numero2 = parseFloat(document.getElementById('valor-dois').value);
-        
-        if (numero2 > numero1){
-            alert("O valor 2 é maior que o valor 1 ");
-        }else 
-            if(numero1 > numero2)
-            alert("O valor 2 é menor que o valor 1 ");
-            else
-            alert("Os valores são iguais");
-            document.getElementById('valor-um').value='';
-            document.getElementById('valor-dois').value='';            
+        const mensagemErro = 'O valor 2 precisa ser maior que o valor 1';
+        const mensagemPositiva = 'O valor 2 é maior que o valor 1';
+        const valorhum = document.getElementById ('valor-um');
+        const valordois = document.getElementById ('valor-dois');        
 
+        if (numero2 > numero1) {
+            const conteudomensagempositiva = document.querySelector('.mensagemPositiva');
+            conteudomensagempositiva.innerHTML = mensagemPositiva;
+            conteudomensagempositiva.style.display = 'block';
+            
+            valorhum.value = '';
+            valordois.value = '';
+            mensagemPositiva = '';
+
+        }else {
+            const conteudomensagemerro = document.querySelector('.mensagemErros');
+            conteudomensagemerro.innerHTML = mensagemErro;
+            conteudomensagemerro.style.display = 'block';
+            mensagemErro = '';
+
+        }
     })            
 
 console.log(form);
